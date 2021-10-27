bb4f0fdf1f0c64f4f7a12be63e4a104e7a5ea2d4
an288705 <aj0466012@gmail.com>
2021-10-27 19:58:24 -0400

    react router

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # BikeShare
 Front end
+
+# dependencies
+npx create-react-app    //react app is already created
+npm install react-scripts@latest
+npm install react-router-dom
--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
 
 function App() {
   /* Set up routing in App.js */
   return (
-    <div>
-      Front end
-    </div>
+    <Router>
+      <div>
+        Front end
+      </div>
+    </Router>
   );
 }
 
