b18a15155a976f8c0b29e55dab76768d7201bfa4
an288705 <aj0466012@gmail.com>
2021-10-28 01:02:56 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 import HomeScreen from './View/home/homeScreen';
-import Navbar from './View/navbar/navbar';
 
 function App() {
   /* Set up routing at app level in App.js */
