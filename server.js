4533a26c8a95e9d4ffc1d27d92424fc86aecda28
Karan Patel <karanpatel7301@gmail.com>
2021-11-02 01:02:54 -0400

    connection to DB

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const dotenv= require("dotenv").config();
-const path = require('path');           
+require("dotenv").config();
 const express = require('express');
-app.set('port', (process.env.PORT || 5000));
-const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const app = express();
+const PORT = process.env.PORT || 3500;
+const bodyParser = require('body-parser');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true})
+.then((result) => console.log('connected to db'))
+.catch((err) => console.log('error is' + err));
 app.use(cors());
 app.use(bodyParser.json());
 {
