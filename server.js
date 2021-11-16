0d75da4b41450c9b9c26cedc746b66112ab309b7
Karan Patel <karanpatel7301@gmail.com>
2021-11-15 19:30:48 -0500

    initial commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 const path = require('path');
 app.use(cors())
 app.use(bodyParser.json())
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true})
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true})
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to db'))
