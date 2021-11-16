8a114c73cd4eb240d324cc7b07d7d73170e28c81
Karan Patel <karanpatel7301@gmail.com>
2021-11-16 15:44:19 -0500

    update heroku

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = 3000;
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const path = require('path');
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true})
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
@@ -14,14 +15,15 @@ db.once('open', () => console.log('Connected to db'))
 
 
 app.use(express.json())
-app.use(express.urlencoded({extended: false}));
 
+app.use(express.urlencoded({extended: false}));
+app.use(express.static('build'));
 if (process.env.NODE_ENV === "production"){
-  app.use(express.static("build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+  app.get('/', (req, res) => {
+    res.send('Hello World');
   });
 }
+
 const announcmentsRouter = require('./routes/announcementsRoute')
 app.use('/announcements',announcmentsRouter)
 const bikesRouter = require('./routes/bikesRoute')
@@ -31,4 +33,4 @@ app.use('/repairs',repairsRouter)
 const usersRouter = require('./routes/usersRoute')
 app.use('/users',usersRouter)
 
-app.listen(process.env.PORT, () => console.log('Server Started'))
+app.listen(process.env.PORT|| port, () => console.log('Server Started'))
