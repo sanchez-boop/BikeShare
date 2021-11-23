e78821ab75f70f9a905a53aa24bae9a189eed7a2
KeiferW <89943031+KeiferW@users.noreply.github.com>
2021-11-23 02:32:04 -0500

    Update user.js

--- models/user.js
+++ models/user.js
@@ -13,10 +13,6 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    id: {
-        type: String,
-        required: true
-    },
     lastName: {
         type: String,
         required: true
