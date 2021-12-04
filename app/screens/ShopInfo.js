4444f2eb2e438eb14d6160a4bfc97a4b6db3e432
vpurohith <vpurohith@gmail.com>
2021-12-04 00:58:47 -0500

    Shop Info Page, needs improvements

--- app/screens/ShopInfo.js
+++ app/screens/ShopInfo.js
@@ -1,15 +1,253 @@
 import { StatusBar } from "expo-status-bar";
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView, FlatList, SafeAreaView, Linking } from "react-native";
 import colors from "../config/colors";
 
+const AnnouncementsHolder = [
+  {
+    id: 1,
+    dateDay: 10,
+    dateMonth: 29,
+    dateYear: 21,
+    message: 'Welcome all incoming bike enthusiasts! Due to UCF Homecoming week, we will be closing at 4:00pm instead of the usual time.',
+  },
+  {
+    id: 2,
+    dateDay: 10,
+    dateMonth: 11,
+    dateYear: 21,
+    message: 'Due to a sudden fire, we will not be able to rent out bikes for the week.',
+  },
+  {
+    id: 3,
+    dateDay: 10,
+    dateMonth: 3,
+    dateYear: 21,
+    message: 'Before coming to the shop, please be sure to sign your waivers for a smoother rental process!',
+  },
+  {
+    id: 4,
+    dateDay: 9,
+    dateMonth: 1,
+    dateYear: 21,
+    message: 'Happy September everyone! Remember you can bring your bikes into the shop for a repair.',
+  },
+  {
+    id: 5,
+    dateDay: 8,
+    dateMonth: 21,
+    dateYear: 21,
+    message: 'Bike safe everyone! Always wear your helmet.',
+  },
+  {
+    id: 6,
+    dateDay: 8,
+    dateMonth: 12,
+    dateYear: 21,
+    message: 'This is a test announcement. ',
+  },
+];
+
+const bikeHours = [
+  {
+    id: 1,
+    inText: 'Monday - Friday: 8am - 5pm'
+  },
+  {
+    id: 2,
+    inText: 'Saturday - Sunday: Closed'
+  } 
+]
+
+const socMedia = [
+  {
+    id: 1,
+    inText: 'FaceBook: https://www.facebook.com/bikengold/',
+    press: 'https://www.facebook.com/bikengold/'
+  },
+  {
+    id: 2,
+    inText: 'Instagram: https://www.instagram.com/ucfbikengold/?hl=en',
+    press: 'https://www.instagram.com/ucfbikengold/?hl=en'
+  }
+]
+
+
 export default function ShopInfo(props) {
+  const renderAnnouncement = ({ item }) => {
+    return (
+      <Announcement
+        item={item}
+        id={item.id}
+        dateDay={item.dateDay}
+        dateMonth={item.dateMonth}
+        dateYear={item.dateYear}
+        message={item.message}
+      />
+    );
+  };
+
+  const Announcement = ({ id, dateDay, dateMonth, dateYear, message}) => (
+    <View style={[styles.itemAnnouncement]}>
+      <View style={styles.itemAnnouncementContainer}>
+        <View>
+          <Text style={[styles.itemText]}>{id}</Text>
+        </View>
+        <Text style={[styles.itemText]}>
+          {message}
+        </Text>
+      </View>
+    </View>
+  );
+
+  const renderHour = ({ item }) => {
+    return (
+      <Hour
+        item={item}
+        id={item.id}
+        inText={item.inText}
+      />
+    );
+  };
+
+  const Hour = ({ id, inText}) => (
+    <View style={[styles.item]}>
+      <View style={styles.itemContainer}>
+        <Text style={[styles.itemText]}>
+          {inText}
+        </Text>
+      </View>
+    </View>
+  );
+
+  const renderMedia = ({ item }) => {
+    const color = '#0645AD';
+    return (
+      <Media
+        item={item}
+        id={item.id}
+        textColor={{ color }}
+        inText={item.inText}
+        press={item.press}
+      />
+    );
+  };
+
+  const Media = ({ id, inText, press, textColor}) => (
+    <View style={[styles.item]}>
+      <View style={styles.itemContainer}>
+        <Text style={[styles.itemText, textColor]} onPress={() => Linking.openURL(press)}>
+          {inText}
+        </Text>
+      </View>
+    </View>
+  );
+
+  const ItemSeparatorView = () => {
+    return (
+      // Flat List Item Separator
+      <View
+        style={{
+          height: 1,
+          width: "100%",
+          backgroundColor: "#DFDFDF",
+        }}
+      />
+    );
+  };
+
+  const AnnouncementHeader = () => {
+    //View to set in Header
+    return (
+      <View>
+        <View style={styles.listHeaderStyle}>
+          <View style={styles.headerTextStyle}>
+            <Text style={styles.itemHeaderText}>Admin Announcements</Text>
+          </View>
+        </View>
+        <View
+          style={{
+            height: 1,
+            width: "100%",
+            backgroundColor: "#DFDFDF",
+          }}
+        />
+      </View>
+    );
+  };
+
+  const HoursHeader = () => {
+    //View to set in Header
+    return (
+      <View>
+        <View style={styles.listHeaderStyle}>
+          <View style={styles.headerTextStyle}>
+            <Text style={styles.itemHeaderText}>Bike Shop Hours</Text>
+          </View>
+        </View>
+        <View
+          style={{
+            height: 1,
+            width: "100%",
+            backgroundColor: "#DFDFDF",
+          }}
+        />
+      </View>
+    );
+  };
+
+  const MediaHeader = () => {
+    //View to set in Header
+    return (
+      <View>
+        <View style={styles.listHeaderStyle}>
+          <View style={styles.headerTextStyle}>
+            <Text style={styles.itemHeaderText}>Social Media</Text>
+          </View>
+        </View>
+        <View
+          style={{
+            height: 1,
+            width: "100%",
+            backgroundColor: "#DFDFDF",
+          }}
+        />
+      </View>
+    );
+  };
+  
   return (
     <View style={styles.mainView}>
       <View style={styles.topRectangle}>
-        <Text style={styles.headerText}>Info</Text>
+        <Text style={styles.headerText}>Bike Shop Info</Text>
       </View>
-
+      <ScrollView style={styles.content}>
+        <SafeAreaView style={styles.announcements}>
+          <FlatList
+            nestedScrollEnabled
+            data={AnnouncementsHolder}
+            renderItem={renderAnnouncement}
+            keyExtractor={(item) => item.id}
+            ItemSeparatorComponent={ItemSeparatorView}
+            ListHeaderComponent={AnnouncementHeader}/>
+        </SafeAreaView>
+        <View style={styles.shopHours}>
+          <FlatList
+              data={bikeHours}
+              renderItem={renderHour}
+              keyExtractor={(item) => item.id}
+              ItemSeparatorComponent={ItemSeparatorView}
+              ListHeaderComponent={HoursHeader}/>
+        </View>
+        <View style={styles.socialMedia}>
+          <FlatList
+            data={socMedia}
+            renderItem={renderMedia}
+            keyExtractor={(item) => item.id}
+            ItemSeparatorComponent={ItemSeparatorView}
+            ListHeaderComponent={MediaHeader}/>
+        </View>
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
@@ -34,5 +272,75 @@ const styles = StyleSheet.create({
     fontFamily: "HindVadodara_600SemiBold",
     fontSize: 20,
     marginBottom: 4,
+  },
+  content: {
+
+  },
+  announcements: {
+    flex: 1,
+    width: "95%",
+    marginTop: '6%',
+    height: 400,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#dfdfdf",
+    marginLeft: 10,
+  },
+  shopHours: {
+    marginTop: '6%',
+    width: "95%",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#dfdfdf",
+    marginLeft: 10,
+  },
+  socialMedia: {
+    marginTop: '6%',
+    marginBottom: '6%',
+    width: "95%",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#dfdfdf",
+    marginLeft: 10,
+  },
+  itemAnnouncement: {
+    paddingVertical: 18,
+  },
+  item: {
+    paddingVertical: 14,
+  },
+  listHeaderStyle: {
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+    width: "100%",
+    height: 40,
+    backgroundColor: "#f2f2f2",
+    justifyContent: "center",
+  },
+  headerTextStyle: {
+    color: "#000000",
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  itemHeaderText: {
+    fontFamily: "Inter_600SemiBold",
+    fontSize: 15.5,
+  },
+  itemAnnouncementContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingLeft: 40,
+    paddingRight: 26,
+    //backgroundColor: "red",
+  },
+  itemContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingLeft: 20,
+    paddingRight: 20,
+    //backgroundColor: "red",
+  },
+  itemText: {
+    fontSize: 12.5,
   },
 });
--- app/screens/Waiver.js
+++ app/screens/Waiver.js
@@ -29,41 +29,61 @@ export default function Waiver(props) {
         <View style={{ padding: 20 }}>
           <Text style={styles.waiverFormTitle}>BikeShare Liability Form</Text>
           <Text style={styles.waiverText}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            malesuada, tellus sed auctor varius, nulla orci porttitor arcu,
-            accumsan ultricies magna nulla ac lacus. Morbi quis auctor orci.
-            Donec non justo rhoncus, venenatis sem ut, ornare mi. Ut facilisis
-            vulputate euismod. Suspendisse potenti. Aliquam ac nunc sit amet
-            odio placerat fermentum nec quis risus. Duis ligula nisi, lobortis
-            vitae libero feugiat, posuere tempor quam. Curabitur lorem massa,
-            accumsan non lacus in, mattis scelerisque metus. Quisque porta diam
-            sed metus efficitur condimentum. Aenean sollicitudin eros ut augue
-            tristique, eu mattis enim egestas.
-            {"\n"}
-            {"\n"}
-            Aliquam ultrices arcu orci, id pretium enim consectetur in.
-            Pellentesque ornare purus ut tempor rhoncus. Proin vehicula turpis
-            sit amet augue venenatis congue eu et risus. Suspendisse est justo,
-            maximus eget eros ut, feugiat accumsan nibh. Sed commodo, augue
-            molestie suscipit venenatis, velit purus luctus mi, eget porttitor
-            mauris augue ut Lorem ipsum dolor sit amet, consectetur adipiscing
-            elit. Donec malesuada, tellus sed auctor varius, nulla orci
-            porttitor arcu, accumsan ultricies magna nulla ac lacus. Morbi quis
-            auctor orci. Donec non justo rhoncus, venenatis sem ut, ornare mi.
-            Ut facilisis vulputate euismod. Suspendisse potenti. Aliquam ac nunc
-            sit amet odio placerat fermentum nec quis risus. Duis ligula nisi,
-            lobortis vitae libero feugiat, posuere tempor quam. Curabitur lorem
-            massa, accumsan non lacus in, mattis scelerisque metus. Quisque
-            porta diam sed metus efficitur condimentum. Aenean sollicitudin eros
-            ut augue tristique, eu mattis enim egestas.
-            {"\n"}
-            {"\n"}
-            Aliquam ultrices arcu orci, id pretium enim consectetur in.
-            Pellentesque ornare purus ut tempor rhoncus. Proin vehicula turpis
-            sit amet augue venenatis congue eu et risus. Suspendisse est justo,
-            maximus eget eros ut, feugiat accumsan nibh. Sed commodo, augue
-            molestie suscipit venenatis, velit purus luctus mi, eget porttitor
-            mauris augue ut
+          I understand that there are risks and dangers inherent in participating and/or receiving instruction in the UCF Student Union Bike Repair, hereinafter "Activity". I also understand that in order to be allowed to participate and/or receive instruction in Activity I must give up my rights to hold The University of Central Florida liable for any injury or damage which I may suffer while participating and/or receiving instruction in Activity.
+Knowing this, and in consideration of being permitted to participate and/or receive instruction in Activity, I hereby voluntarily release the Sponsor, The University of Central Florida, The UCF Board of Trustees, The State of Florida, and The Florida Board of Governors from any and all liability resulting from or arising out of my participation and/or receipt of instruction in Activity.
+I understand and agree that I am releasing not only the entities set forth in the paragraph above, but also the officers, agents, and employees of those entities, hereinafter known as Representatives.
+I understand and agree that by signing this Waiver/Release, I am assuming full responsibility for any and all risk of death or personal injury or property damage suffered by me while participating and/or receiving instruction in Activity.
+I, individually, and on behalf of my heirs, successors, assigns and personal representatives, hereby release, acquit and forever discharge the released entities listed above and their Representatives from any and all liability and claims whatsoever, including negligence on behalf of a Representative, for any and all damages, losses or injuries I sustain to my person or property, or both; including but not limited to any claims, demands, actions, causes of action, damages, costs, expenses and/or attorney’s fees which arise out of, occur during or occur in connection with my participation and/or instruction in the Activity.
+I, individually, and on behalf of my heirs, successors, assigns and personal representatives, hereby agree to indemnify, defend and hold harmless the University and its Representatives (in their official and individual capacities) from any and all liability, loss or damage they or any of them incur or sustain as a result of any claims, demands, actions, causes of action, judgments, costs or expenses, including attorney’s fees, which result from, occur during, or arise out of my participation and/or instruction in the Activity.
+I understand that this Waiver and Release Agreement is to be construed under the laws of the State of Florida that it is intended to be as broad and inclusive as permitted by the laws of the State of Florida and that if any portion hereof is held invalid, the balance hereof shall, notwithstanding, continue in full legal force and effect.
+I acknowledge that I have carefully read this waiver/release agreement, that I am 18 years of age or older, I understand its contents and purposes, and voluntarily agree to all the terms set forth for participation and/or instruction in Activity.
+This applies to all services below as well as all other rendered services:
+•	       Brake adjustment
+•	       Pump up tires
+•	       Gear (derailleur) adjustment
+•	       Change flat tube
+•	       Tighten loose part(s)
+•	       Part(s) replacement
+•	       Bottom bracket adjustment
+•	       Wheel truing
+•	       Hub adjustment
+•	       Chain maintenance
+•	       Bike advice    
+By agreeing with the terms and conditions, you agree to the above statements. 
+
+
+SG Bike Share – Rental Agreement
+ 
+GENERAL INFORMED CONSENT AND RELEASE: BIKE SHARE AGREEMENT:
+ 
+I, the undersigned, recognize that participation in the referenced activity is strictly voluntary and that such participation does not in any manner establish an employer-employee or an agency relationship with the Sponsor or the University of Central Florida.
+ 
+I, the undersigned, in consideration of the request and permission to participate in the referenced activity hereby assume full responsibility for all risk of injury or loss which may result from my participation in the activity and hereby agree to hold harmless, release and forever discharge the Sponsor and the University of Central Florida, their officers, agents, and employees from any and all claims and demands whatsoever which the undersigned or any representative of the undersigned, may have by reason of any accident, illness, injury to or death of any person or persons, or damage to or loss or destruction of any property arising or resulting directly or indirectly from participation in the aforementioned and occurring during said participation or any time subsequent thereto, save and except that the above provisions shall not be applicable to injury to or death of person, or damage to or loss of
+property arising out of the negligent or intentional acts of the Sponsor or the University of Central Florida. The terms of this release will serve as a release and assumption of risk for my heirs, executors and administrators for all my family members.
+ 
+I agree and acknowledge that some activities may be of a hazardous nature and /or include physical and/or strenuous exercise or activity and, understanding this, I state that to the best of my knowledge I have no medical, physical or mental health conditions that would hinder or prevent my active participation in the referenced activity.
+ 
+PLEASE NOTE: It is strongly recommended that each participant maintain some type of accident medical insurance for his/her own protection.
+I further agree to cover the cost of any lost equipment (while in my care) at the following rates:
+•         Loss of lock and/or key $50.00 (key loss requires lock to be replaced).
+•         Loss of Sign $20.00
+•         Loss of bicycle $350.00
+UCF BIKE SHARE TERMS AND CONDITIONS
+ 
+1. CONDITION: Bicycle and lock must be returned in the same conditions as when issued to user.
+2. AGE RESTRICTION: Must be a minimum of 18 years old to participate in this program.
+By signing this waiver form, user acknowledges he/she is at least 18 years old.
+3. NO WARRANTY: The University of Central Florida makes no warranties express or implied including the warranties of merchantability and fitness for a particular purpose.
+4. BICYCLE RESTRICTED USE REQUIREMENTS:
+•         Bike for user only: User agrees that only he/she will be riding the bicycle during the term of this bicycle agreement.
+•         Helmet use: It is highly recommended that a bicycle helmet (that meets the Consumer Product Safety Commission for protective headgear) is worn while riding this loaner bicycle.
+5. FINES OR VIOLATIONS: User acknowledges that all traffic and parking citations received in the use of this bicycle are the sole responsibility of the user.
+6. RETURN: Bicycle and lock must be returned by the end of the loan period. User will be charged a $10.00/day late fee. After 10 business days user will be charged the full cost of the bicycle ($350), sign ($20) and rented equipment ($50 for lock and/or key). Failure to return bicycle, helmet and lock will subject user to legal liability of $420.00, as well as having student account placed on hold.
+
+
+By agreeing with these terms and conditions, you agree to take responsibility for the bicycle while it's checked out and agree to report any problems, mechanical or otherwise upon returning the bicycle. You confirm that you 18 years of age (or older) and agree to all the terms and conditions of this agreement AND that you consent that (if a student) UCF may charge my student account and (if an employee) UCF may collect through payroll deduction, all associated fees (as listed above) for non-compliance with this agreement.
+
+
           </Text>
           <TextInput
             style={styles.waiverSign}
