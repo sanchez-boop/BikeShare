5ad25143bec17b1abc75126c02a970f54aa806c1
KeiferW <89943031+KeiferW@users.noreply.github.com>
2021-12-06 19:43:31 -0500

    Update repairsRoute.js

--- routes/repairsRoute.js
+++ routes/repairsRoute.js
@@ -1,22 +1,19 @@
 const express = require('express')
 const router = express.Router()
 const Repair = require('../models/repair')
-//jwt auth middleware
-const jwt = require('jsonwebtoken');
-const auth1 = require('./authenticate')
 
 //getting all repairs 
-router.get('/', auth1, async (req, res) => {
+router.get('/', async (req, res) => {
     try {
         const repairs = await Repair.find()
         res.json(repairs)
     } catch (err){
-        res.status(500).json({ message : err.message })
+        res.satus(500).json({ message : err.message })
     }
 })
 
 //create a repair
-router.post('/', auth1, async (req, res) => {
+router.post('/', async (req, res) => {
     const repair = new Repair({
         bikeModel: req.body.bikeModel,
         customerID: req.body.customerID,
@@ -35,10 +32,10 @@ router.post('/', auth1, async (req, res) => {
     }
 })
 //updating a repair
-router.patch('/', auth1, async (req, res) => {
+router.patch('/', async (req, res) => {
     try{
         const updatedRepair = await Repair.updateOne(
-            {_id: req.body._id},
+            {id: req.body.id},
             { $set: {           bikeModel: req.body.bikeModel,
                                 customerID: req.body.customerID,
                                 id: req.body.id,
@@ -48,14 +45,14 @@ router.patch('/', auth1, async (req, res) => {
                                 name: req.body.name,
                                 phone:req.body.phone } }
             )
-            const retVal = await Repair.findOne({_id:req.body._id})
+            const retVal = await Repair.findOne({id:req.body.id})
             res.json(retVal)
     }catch(err){
         res.status(400).json({message: err.message})
     }
 })
 //deleteing a user
-router.delete('/', auth1, async (req, res) => {
+router.delete('/', async (req, res) => {
     try{
     const deletedRepair = await Repair.deleteOne({id: req.body.id})
     res.json(deletedRepair)
@@ -64,12 +61,12 @@ router.delete('/', auth1, async (req, res) => {
     }
 })
 //searching repairs 
-router.post('/search',  auth1, async (req, res, next) =>
+router.post('/search', async (req, res, next) =>
 {
 try{
     const searchedRepairs = await Repair.find({$or:[{name:{$regex: req.body.key, $options: 'i'}},
-                                                    {bikeModel:{$regex: req.body.key, $options: 'i'}},
-                                                    {phone:{$regex: req.body.key, $options: 'i'}}]})
+                                                {bikeModel:{$regex: req.body.key, $options: 'i'}},
+                                                {phone:{$regex: req.body.key, $options: 'i'}}]})
     res.json(searchedRepairs)
 }catch(err){
     res.status(400).json({message: err.message })
