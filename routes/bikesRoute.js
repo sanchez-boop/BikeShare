a77d3f38f3143201ec1e5317858eeaa472ccdab2
KeiferW <89943031+KeiferW@users.noreply.github.com>
2021-12-07 21:42:49 -0500

    Update bikesRoute.js

--- routes/bikesRoute.js
+++ routes/bikesRoute.js
@@ -94,7 +94,7 @@ router.post('/search3', async (req, res, next) =>
 try{
     const searchedBikes = await Bike.find({
         $and: [
-            {dateRented:{$gte:""}},
+            {dateRented:{$gte:" "}},
             {$or:[
                 {name:{$regex: req.body.key, $options: 'i'}},
                 {id:{$regex: req.body.key, $options: 'i'}}
